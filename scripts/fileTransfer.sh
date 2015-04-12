35b68356f42544b05d3ccc29b6384630802e44f1
Nathan Shaw <nathanshawsemail@gmail.com>
2015-04-11 17:40:12 -0700

    updated things

--- scripts/fileTransfer.sh
+++ scripts/fileTransfer.sh
@@ -8,7 +8,7 @@
 
 clear
 echo copying files to macBook
-sshpass -p "ns098930" scp -r /home/pi/cubistpi/pictures nathan@10.0.1.13:/users/nathan/desktop/pictures
+sshpass -p "ns098930" scp -r /home/pi/cubistpi/pictures nathan@10.0.1.13:/users/nathan/desktop/cubistPics
 
 ls -t | sed -e '1,50d' | xargs -d '\n' rm
 echo "Geting rid of Extra Files"
